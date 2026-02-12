a422ec925c7b9607f878bec9f812a08a0ad69673
Nikhil Salunkhe <nikkk0914@users.noreply.huggingface.co>
2026-02-12 11:38:06 +0000

    Update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -99,4 +99,4 @@ async def get_specific_standard(standard_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
